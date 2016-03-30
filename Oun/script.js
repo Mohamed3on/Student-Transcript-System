c0d68d54257486aa1c9672c0b1a333eaf036a26a
Mohamed Oun <Mohamed Oun>
2016-03-30 23:06:20 +0200

    30 march

--- Oun/script.js
+++ Oun/script.js
@@ -1,10 +1,14 @@
 /**
  * Created by Mohamed Oun on 30-Mar-16.
  */
-var name=prompt("Please enter your name");
-function setName() {
-    if (name !=null)
-        document.getElementById("name").innerHTML =
-            name;
+var name = "Mohamed";
+var id = 124897, year = "Third Year",faculty="Computer Science";
+function setValues() {
+    if (name != null)
+        document.getElementById("name").innerHTML = name;
+    document.getElementById("ID").innerHTML = id;
+    document.getElementById("year").innerHTML = year;
+    document.getElementById("faculty").innerHTML = faculty;
 }
-document.addEventListener('DOMContentLoaded',setName,false);
+
+document.addEventListener('DOMContentLoaded', setValues, false);
