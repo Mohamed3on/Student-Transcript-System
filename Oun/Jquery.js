22da95d0cbc34db7c9619d6b6862ba9afa30494e
Mohamed Oun <Mohamed Oun>
2016-03-31 13:19:57 +0200

    added checkboxes using Javascript

--- Oun/Jquery.js
+++ Oun/Jquery.js
@@ -1,7 +1,3 @@
 /**
  * Created by Mohamed Oun on 30-Mar-16.
  */
-$(function(){
-    $("#header").load("header.html #nav");
-    $("#footer").load("header.html #footer");
-});
--- Oun/script.js
+++ Oun/script.js
@@ -1,14 +1,53 @@
 /**
  * Created by Mohamed Oun on 30-Mar-16.
  */
+//Jquery
+$(function(){
+    $("#header").load("header.html #nav");
+    $("#footer").load("header.html #footer");
+});
+
 var name = "Mohamed";
 var id = 124897, year = "Third Year",faculty="Computer Science";
+
 function setValues() {
     if (name != null)
         document.getElementById("name").innerHTML = name;
     document.getElementById("ID").innerHTML = id;
     document.getElementById("year").innerHTML = year;
     document.getElementById("faculty").innerHTML = faculty;
+
 }
 
-document.addEventListener('DOMContentLoaded', setValues, false);
+function addCheckbox() {
+    var table= document.getElementById("GradesTable");
+    var x = table.rows[0].insertCell(0);
+    x.innerHTML = "<th>Select</th>";
+    for ( i=1; i<table.rows.length; i++)
+    {
+        var x = table.rows[i].insertCell(0);
+        x.innerHTML = "<input  type='checkbox' class='checkbox' id='checkbox"+i+"'>";
+    }
+
+}
+function CheckChecked() {
+    var table= document.getElementById("GradesTable");
+    $(".checkbox").click(function(){
+    for ( i=1; i<=table.rows.length; i++)
+    {
+
+            if( $("#checkbox"+i).is(":checked"))  document.getElementById("modules").innerHTML=i+"<br>";
+
+        }
+
+
+    }
+    );
+}
+
+function init() {
+    setValues();
+    addCheckbox();
+    CheckChecked();
+
+}
