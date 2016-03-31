4826ed4673667c7badfc764b89c36b08bc9d0f4d
Mohamed Oun <Mohamed Oun>
2016-03-31 22:15:24 +0200

    add select all/none

--- Oun/script.js
+++ Oun/script.js
@@ -21,23 +21,34 @@ function setValues() {
 
 function addCheckbox() {
     var table = document.getElementById("GradesTable");
-    var x = table.rows[0].insertCell(0);
+    var x = table.rows[1].insertCell(0);
     x.innerHTML = "<th>Select</th>";
-    for (i = 1; i < table.rows.length; i++) {
+    for (i = 2; i < table.rows.length; i++) {
         var x = table.rows[i].insertCell(0);
-        x.innerHTML = "<input  type='checkbox' class='checkbox' id='checkbox" + i + "'>";
+        x.innerHTML = "<input checked='checked'  type='checkbox' class='checkbox' id='checkbox" + i + "'>";
     }
 
 }
 function CheckChecked() {
     var table = document.getElementById("GradesTable");
+    document.getElementById("modules").innerHTML = "";
+    for (i = 1; i < table.rows.length; i++) {
+        var $row = $(table.rows[i].cells[2]).text();
+        if ($("#checkbox" + i).is(":checked"))
+            $("#modules").append("<li>" + $row + "</li>");
+
+    }
+
+}
+function CheckCheckedOnClick() {
+
     $(".checkbox").click(function () {
-        document.getElementById("modules").innerHTML="";
-        var $modules= document.getElementById("modules");
+            var table = document.getElementById("GradesTable");
+            document.getElementById("modules").innerHTML = "";
             for (i = 1; i < table.rows.length; i++) {
                 var $row = $(table.rows[i].cells[2]).text();
                 if ($("#checkbox" + i).is(":checked"))
-                  $(modules).append( "<li>" + $row + "</li>");
+                    $("#modules").append("<li>" + $row + "</li>");
 
             }
 
@@ -45,27 +56,61 @@ function CheckChecked() {
         }
     );
 }
-$(document).ready(function() {
-    $("#average").click(function () {
+function getAverage() {
+    {
         var table = document.getElementById("GradesTable");
         var count = 0, sum = 0, average;
         for (i = 1; i < table.rows.length; i++) {
             var $row = $(table.rows[i].cells[6]).text();
-            var num=parseInt($row);
+            var num = parseInt($row);
             if ($("#checkbox" + i).is(":checked")) {
                 count++;
                 sum += num;
             }
         }
         average = sum / count;
-        document.getElementById("averageResult").innerHTML = average;
+        if (count != 0)
+            document.getElementById("averageResult").innerHTML = average;
+        else
+            document.getElementById("averageResult").innerHTML = "No Courses selected";
 
+    }
+}
+$(document).ready(function () {
+    $("#average").click(function () {
+        getAverage();
     });
+    $("#selectAll").click(function () {
+        $(".checkbox").prop('checked', $(this).prop("checked"));
+        CheckChecked();
+    });
+    $("#selectNone").click(function () {
+        $('.checkbox').each(function () { //loop through each checkbox
+            this.checked = false; //deselect all checkboxes with class "checkbox1"
+        });
+        CheckChecked();
+    });
+    var table = document.getElementById("GradesTable");
+    $(".checkbox").click(function () {
+            document.getElementById("modules").innerHTML = "";
+            var $modules = document.getElementById("modules");
+            for (i = 1; i < table.rows.length; i++) {
+                var $row = $(table.rows[i].cells[2]).text();
+                if ($("#checkbox" + i).is(":checked"))
+                    $(modules).append("<li>" + $row + "</li>");
+
+            }
+
+
+        }
+    );
 });
 
 function init() {
     setValues();
     addCheckbox();
+    CheckCheckedOnClick();
     CheckChecked();
+    getAverage();
 
 }
