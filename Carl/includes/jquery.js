a209e87c69bcd5c66b20b81d376888c247315690
Carl Eqladios <ceqladios@gmail.com>
2016-03-31 21:38:43 +0200

    Organize folders

---
+++ Carl/includes/jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#header").load("includes/header.html");
+    $("#header-logged").load("includes/header-logged.html");
+    $("#footer").load("includes/footer.html" ); 
+});
