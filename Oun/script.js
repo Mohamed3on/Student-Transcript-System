a39b90f0708b63f323f913c66cdaba8f3783247b
Mohamed Oun <Mohamed Oun>
2016-03-31 17:46:17 +0200

    Added checkboxes to choose modules and get average

--- Oun/script.js
+++ Oun/script.js
@@ -2,13 +2,13 @@
  * Created by Mohamed Oun on 30-Mar-16.
  */
 //Jquery
-$(function(){
+$(function () {
     $("#header").load("header.html #nav");
     $("#footer").load("header.html #footer");
 });
 
 var name = "Mohamed";
-var id = 124897, year = "Third Year",faculty="Computer Science";
+var id = 124897, year = "Third Year", faculty = "Computer Science";
 
 function setValues() {
     if (name != null)
@@ -20,30 +20,48 @@ function setValues() {
 }
 
 function addCheckbox() {
-    var table= document.getElementById("GradesTable");
+    var table = document.getElementById("GradesTable");
     var x = table.rows[0].insertCell(0);
     x.innerHTML = "<th>Select</th>";
-    for ( i=1; i<table.rows.length; i++)
-    {
+    for (i = 1; i < table.rows.length; i++) {
         var x = table.rows[i].insertCell(0);
-        x.innerHTML = "<input  type='checkbox' class='checkbox' id='checkbox"+i+"'>";
+        x.innerHTML = "<input  type='checkbox' class='checkbox' id='checkbox" + i + "'>";
     }
 
 }
 function CheckChecked() {
-    var table= document.getElementById("GradesTable");
-    $(".checkbox").click(function(){
-    for ( i=1; i<=table.rows.length; i++)
-    {
+    var table = document.getElementById("GradesTable");
+    $(".checkbox").click(function () {
+        document.getElementById("modules").innerHTML="";
+        var $modules= document.getElementById("modules");
+            for (i = 1; i < table.rows.length; i++) {
+                var $row = $(table.rows[i].cells[2]).text();
+                if ($("#checkbox" + i).is(":checked"))
+                  $(modules).append( "<li>" + $row + "</li>");
 
-            if( $("#checkbox"+i).is(":checked"))  document.getElementById("modules").innerHTML=i+"<br>";
+            }
+
 
         }
-
-
-    }
     );
 }
+$(document).ready(function() {
+    $("#average").click(function () {
+        var table = document.getElementById("GradesTable");
+        var count = 0, sum = 0, average;
+        for (i = 1; i < table.rows.length; i++) {
+            var $row = $(table.rows[i].cells[6]).text();
+            var num=parseInt($row);
+            if ($("#checkbox" + i).is(":checked")) {
+                count++;
+                sum += num;
+            }
+        }
+        average = sum / count;
+        document.getElementById("averageResult").innerHTML = average;
+
+    });
+});
 
 function init() {
     setValues();
